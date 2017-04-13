b62757bfa6a6887b8cbd85e87b1e5a67768b6cd3
mahdi0011 <gooddevil1062@gmail.com>
2017-04-13 12:08:01 +0430

    Update GetProf.lua

--- plugins/GetProf.lua
+++ plugins/GetProf.lua
@@ -2,7 +2,7 @@
 local function run(msg, matches)
  if matches[1] == "getpro" or matches[1] == "Getpro" or matches[1] == "پروفایل" then
  local function dl_photo(arg,data)
- tdcli.sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, data.photos_[0].sizes_[1].photo_.persistent_id_,'YOUR iD >>'..msg.sender_user_id_..' ') 
+ tdcli.sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, data.photos_[0].sizes_[1].photo_.persistent_id_,'آیدی شما :'..msg.sender_user_id_..' ') 
 end
   tdcli_function ({ID = 
 "GetUserProfilePhotos",user_id_ = 
